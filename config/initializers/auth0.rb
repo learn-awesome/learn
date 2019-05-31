30786685099ff48a7edf4ef8fe07fde4703fd9ef
nilesh <git@nileshtrivedi.com>
2019-06-01 02:38:49 +0530

    use envvars for creds

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -1,7 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider(
     :auth0,
-    'polyglotnetwork.eu.auth0.com',
+    ENV['AUTH0_PUBKEY'],
+    ENV['AUTH0_PRIVKEY'],
+    'learnawesome.eu.auth0.com',
     callback_path: '/auth/oauth2/callback',
     authorize_params: {
       scope: 'openid profile'
