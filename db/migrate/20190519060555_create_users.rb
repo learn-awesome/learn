bc010285e34dd5189c03b7fd01965ea40f0d3c53
nilesh <git@nileshtrivedi.com>
2019-06-05 01:04:20 +0530

    homepage draft

--- db/migrate/20190519060555_create_users.rb
+++ db/migrate/20190519060555_create_users.rb
@@ -1,9 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users, id: :uuid do |t|
-      t.string :nickname
-      t.string :auth0_uid
-      t.text   :authinfo
+      t.string :nickname, null: false
+      t.string :auth0_uid, null: false
+      t.text   :authinfo, null: false
+      t.string :image_url
 
       t.timestamps
     end
