64bffd13ae7f954082ed802d6fab39274347285e
nilesh <git@nileshtrivedi.com>
2019-06-16 12:56:28 +0530

    Redirect to logout if current_user is nil on Dashboard page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,10 @@ class DashboardController < ApplicationController
   include Secured
   
   def show
+  	if current_user.nil?
+  		# Anubhav's error case. Need to debug.
+  		redirect_to("/logout") and return
+  	end
   	@user_topics = current_user.user_topics
   	if @user_topics.blank?
   		flash[:success] = "Follow your favorite topics to get a personalized dashboard."
