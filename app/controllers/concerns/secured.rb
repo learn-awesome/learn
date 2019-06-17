4daa32aef5c0cec687faf1bf02a6f1d248d17736
nilesh <git@nileshtrivedi.com>
2019-06-18 01:44:32 +0530

    After login, redirect user to original_path if available

--- app/controllers/concerns/secured.rb
+++ app/controllers/concerns/secured.rb
@@ -2,7 +2,17 @@ module Secured
   extend ActiveSupport::Concern
 
   def logged_in_using_omniauth?
-  	flash[:danger] = "This action requires loggin in."
-    redirect_to '/' unless session[:userinfo].present?
+    if session[:userinfo].present?
+    	# user logged in. if cookie has original_path then clear it and redirect there
+		if session[:original_path].present?
+			path = session[:original_path]
+			session[:original_path] = nil
+			redirect_to path
+		end
+	else
+		# store in cookie where user wanted to go
+		session[:original_path] = request.fullpath
+		redirect_to '/auth/auth0'
+	end
   end
 end
