e45c72ef7c2c6f2b8924f5038c4c937e2b0d2755
eshnil <team@polyglot.network>
2019-07-10 14:09:08 +0530

    Fix for creating topics

--- app/admin/topics.rb
+++ app/admin/topics.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Topic do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-  permit_params :name, :gitter_room
+  permit_params :name, :gitter_room,:search_index
 #
 # or
 #
