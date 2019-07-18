618d3d8219c8893fd10cb576e84beff501109490
eshnil <team@polyglot.network>
2019-07-18 22:15:00 +0530

    Fixes #53 Basic version of item_types/show page

--- app/controllers/item_types_controller.rb
+++ app/controllers/item_types_controller.rb
@@ -1,5 +1,7 @@
 class ItemTypesController < ApplicationController
 	def show
 		@item_type = ItemType.find(params[:id])
+		@items = Item.where(item_type: @item_type).includes(:topics)
+		@topics = Topic.order(:name).all
 	end
 end
