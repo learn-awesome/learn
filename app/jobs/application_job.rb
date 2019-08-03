5b17467e0ac5387ccbdf6b34a082f0b8ff852a0e
eshnil <team@polyglot.network>
2019-08-03 20:07:18 +0530

    url helper fix in ApplicationJob

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,8 +1,9 @@
 class ApplicationJob < ActiveJob::Base
   include Rails.application.routes.url_helpers
 
-  protected
-  def default_url_options
-    Rails.application.config.active_job.default_url_options
+  class << self
+	  def default_url_options
+	    Rails.application.config.active_job.default_url_options
+	  end
   end
 end
