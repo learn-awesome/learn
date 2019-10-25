c34bbaf10b10302fee31c9b913ca325ec4b5bbd3
eshnil <team@polyglot.network>
2019-10-25 14:04:39 +0530

    WIP: SplitButtonComponent

--- app/components/split_button_component.rb
+++ app/components/split_button_component.rb
@@ -1,4 +1,6 @@
 class SplitButtonComponent < ActionView::Component::Base
+    # To use
+    # <%= render SplitButtonComponent, id_prefix: "sbc", name: "sbc", labels: [["Learned", "learned"], ["Currently learning","learning"], ["Want to learm", "want_to_learn"]], value: @my_review.status %>
     validates :id_prefix, :name, :labels, presence: true
 
     def initialize(id_prefix:, name:, value: nil, labels:, onchange: "", color: "yellow", size: 1.0)
