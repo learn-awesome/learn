dd0f4f61b30895a8e4c987dfed778b217203f8d6
eshnil <team@polyglot.network>
2019-11-10 20:01:50 +0530

    Users listing page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   before_action :logged_in_using_omniauth?, only: [:edit, :update, :toggle_follow, :settings]
 
   def index
+    @users = User.order('RANDOM()').limit(100)
   end
 
   def show
