eebc8711235d68703b42e78618e03f0418719269
eshnil <team@polyglot.network>
2020-01-03 17:16:15 +0530

    WIP #123 Daily email to users to bring them back

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -12,4 +12,11 @@ class UserMailer < ApplicationMailer
 		@follower = params[:follower]
 		mail(to: @user.email, subject: "#{@follower.nickname} has started following you on learnawesome.org")
 	end
+
+	def daily_email(user, new_followers, new_fav_items)
+		return unless user.email.present?
+		@new_followers = new_followers
+		@new_fav_items = new_fav_items
+		mail(to: user.email, subject: "Your daily update from LearnAwesome.org")
+	end
 end
