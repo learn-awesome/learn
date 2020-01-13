2bba253a2b17436b97af8e69f34beb863df092ab
Nidhin D <nidhin.damodaran@weinvest.net>
2020-01-13 21:15:46 +0530

    WIP #95 : fixed issue with duplication and multiple topics issue

--- app/controllers/item_types_controller.rb
+++ app/controllers/item_types_controller.rb
@@ -1,8 +1,7 @@
 class ItemTypesController < ApplicationController
 	def show
 		@item_type = ItemType.find(params[:id])
-		@topics = Topic.joins(items: :item_type).where(item_types: {id: @item_type.id}).order(:name).includes(:items).paginate(page: params[:page])
+		@topics = Topic.eager_load(:items).where("items.item_type_id=?", @item_type.id).order(:name).paginate(page: params[:page])
 		flash.now[:danger] = "No Topics and Items" if @topics.blank?
-
 	end
 end
