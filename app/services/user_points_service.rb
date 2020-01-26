1ff0bc50c5fd12725dddce9e14bbf5093f794518
Nidhin D <nidhin.damodaran@weinvest.net>
2020-01-26 17:24:24 +0530

    WIP #133 Code indendation

--- app/services/user_points_service.rb
+++ app/services/user_points_service.rb
@@ -1,21 +1,21 @@
 class UserPointsService < BaseService
 	def initialize(user: , event: )
-		@user = user
-		@event = event
+	  @user = user
+	  @event = event
 	end
 
 	def call
-		add_event_score
-		user.save
+	  add_event_score
+	  user.save
 	end
 
 	private
 
-		attr_accessor :user, :event
+	  attr_accessor :user, :event
 
-		POINT_SYSTEM = {submission: 10, reviews: 10, referral: 50}
+    POINT_SYSTEM = {submission: 10, reviews: 10, referral: 50}
 
-		def add_event_score
-			user.score += POINT_SYSTEM[event]
-		end
+	  def add_event_score
+	   user.score += POINT_SYSTEM[event]
+	  end
 end
