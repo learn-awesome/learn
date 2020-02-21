c0e63a1bc7ecfedbc2f924975ce6bc8b0637ac4a
eshnil <team@polyglot.network>
2020-02-21 22:58:00 +0530

    Fix #139 Delete duplicates before running the migration

--- db/migrate/20200213173853_add_uniq_index_to_topic_idea_set.rb
+++ db/migrate/20200213173853_add_uniq_index_to_topic_idea_set.rb
@@ -1,5 +1,6 @@
 class AddUniqIndexToTopicIdeaSet < ActiveRecord::Migration[6.0]
   def change
+    TopicIdeaSet.dedupe
   	add_index :topic_idea_sets, [:topic_id, :idea_set_id], unique: true
   end
 end
