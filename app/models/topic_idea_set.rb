a354cb5bf049b7dac593e06ff2569fc4c9793a1e
eshnil <team@polyglot.network>
2020-02-21 22:51:17 +0530

    WIP #139 Methods to check and remove duplicate TopicIdeaSet rows

--- app/models/topic_idea_set.rb
+++ app/models/topic_idea_set.rb
@@ -16,4 +16,22 @@ class TopicIdeaSet < ApplicationRecord
 	validates :topic, presence: true
 	validates :idea_set, presence: true
 	validates :topic_id, uniqueness: { scope: :idea_set_id }
+
+	def self.dedupe
+		grouped = all.group_by{|ti| [ti.topic_id, ti.idea_set_id] }
+		grouped.values.each do |duplicates|
+			# the first one we want to keep right?
+			first_one = duplicates.shift # or pop for last one
+			# if there are any more left, they are duplicates
+			# so delete all of them
+			duplicates.each{|double| double.destroy} # duplicates can now be destroyed
+		end
+	end
+
+	def self.check_duplicates
+		grouped = all.group_by{|ti| [ti.topic_id, ti.idea_set_id] }
+		grouped.keys.select do |ti|
+			grouped[ti].size > 1
+		end
+	end
 end
