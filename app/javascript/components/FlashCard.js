b39726797e32371895cecc715483eac2c9aef628
Vishal Telangre <the@vishaltelangre.com>
2020-02-23 17:21:10 +0530

    Add flip animation to FlashCard

--- app/javascript/components/FlashCard.js
+++ app/javascript/components/FlashCard.js
@@ -1,67 +1,112 @@
 import React, { useState } from "react";
+import Flippy, { FrontSide, BackSide } from "react-flippy";
 
 const FlashCard = props => {
   const [isHovered, setIsHovered] = useState(false);
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const Actions = () => (
+    <div style={{ ...styles.actions, ...(isHovered ? {} : styles.hidden) }}>
+      <a href={`/flash_cards/${props.id}/edit`} className="mr-2">
+        <span className="far fa-edit"></span>
+      </a>
+      <a
+        href={`/flash_cards/${props.id}`}
+        data-method="delete"
+        data-confirm="Do you really want to delete the selected flash card?"
+        rel="nofollow"
+        className="text-danger"
+      >
+        <span className="fas fa-trash"></span>
+      </a>
+    </div>
+  );
 
   return (
     <div
-      style={styles.listItem}
+      style={styles.cardContainer}
+      onClick={() => setIsFlipped(!isFlipped)}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <div style={isHovered ? styles.actions : { display: "none" }}>
-        <a href={`/flash_cards/${props.id}/edit`} className="mr-2">
-          <span className="far fa-edit"></span>
-        </a>
-        <a
-          href={`/flash_cards/${props.id}`}
-          data-method="delete"
-          data-confirm="Do you really want to delete the selected flash card?"
-          rel="nofollow"
-          className="text-danger"
-        >
-          <span className="fas fa-trash"></span>
-        </a>
-      </div>
-      <div style={styles.container}>{props.question}</div>
+      <Flippy
+        flipDirection="horizontal"
+        isFlipped={isFlipped}
+        style={styles.card}
+      >
+        <FrontSide style={{ ...styles.side, ...styles.frontSide }}>
+          <Actions />
+          <div style={styles.content}>
+            {props.question}
+          </div>
+        </FrontSide>
+        <BackSide style={{ ...styles.side, ...styles.backSide }}>
+          <Actions />
+          <div style={styles.content}>
+            {props.answer}
+          </div>
+        </BackSide>
+      </Flippy>
     </div>
   );
 };
 
 const styles = {
-  listItem: {
+  cardContainer: {
     display: "flex",
     flex: "1 1 22%",
     flexDirection: "column",
-    height: "300px",
     minWidth: "250px",
     width: "22%",
     maxWidth: "22%",
     marginRight: "2%",
-    marginBottom: "20px",
+    marginBottom: "20px"
+  },
+  card: {
+    display: "flex",
+    flex: 1
+  },
+  side: {
+    display: "flex",
+    flex: 1,
     border: "1px solid #ddd",
     borderRadius: "10px",
     background: "#fff",
     boxShadow: "2px 2px 8px 0px #ccc",
-    overflowY: "scroll",
-    position: "relative"
+    overflowX: "hidden"
   },
-  container: {
+  frontSide: {
+    background: "#fff"
+  },
+  backSide: {
+    background: "#ffd"
+  },
+  content: {
     display: "flex",
     flex: "1",
-    alignItems: "center",
+    alignItems: "flex-start",
     justifyContent: "center",
-    padding: "10px"
+    padding: "20px",
+    width: "100%",
+    height: "300px",
+    overflowY: "auto",
   },
   actions: {
+    zIndex: 2,
     display: "flex",
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
+    margin: "-1rem",
     padding: "10px",
     width: "100%",
     position: "absolute",
-    boxShadow: "0 5px 15px 15px #eee"
+    background: "#fff",
+    transform: "none",
+    boxShadow: "0px 1px 10px 10px #fff"
+  },
+  hidden: {
+    display: "none"
   }
 };
 
