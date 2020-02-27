e2dcff27197bf6125ea2a8fbdcd056f16128a2ec
Vishal Telangre <the@vishaltelangre.com>
2020-02-27 14:42:28 +0530

    MathJax and LaTeX support using KaTeX + Markdown live preview

--- app/javascript/flash_card_editor.js
+++ app/javascript/flash_card_editor.js
@@ -1,14 +1,39 @@
 import SimpleMDE from "simplemde";
+import MarkdownIt from "markdown-it";
+import MarkdownKaTexPlugin from "@zombie110year/markdown-it-katex";
+
 import "simplemde/dist/simplemde.min.css";
+import "katex/dist/katex.min.css";
+import "github-markdown-css/github-markdown.css";
+
+const renderMarkdownPreview = plainText => {
+  const markdownIt = new MarkdownIt({
+    html: true,
+    linkify: true,
+    typographer: true
+  });
+  return markdownIt.use(MarkdownKaTexPlugin).render(plainText);
+};
 
 const defaultEditorConfig = {
   hideIcons: ["guide"],
   showIcons: ["code", "table"],
   status: false,
-  spellChecker: false
+  spellChecker: false,
+  previewRender: renderMarkdownPreview,
+  // Async method
+  previewRender: function(plainText, preview) {
+    setTimeout(function() {
+      preview.innerHTML = renderMarkdownPreview(plainText);
+    }, 250);
+
+    return "Loading...";
+  }
 };
 
 $(() => {
+  if (!document.getElementById("flash_card_form")) return;
+
   const flashcardQuestionEditor = new SimpleMDE({
     ...defaultEditorConfig,
     element: document.getElementById("flash_card_question")
