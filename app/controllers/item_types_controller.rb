c7dc8aeff285c82877fa5c7f4d55fce73a2a8350
Nilesh <git@nileshtrivedi.com>
2020-05-03 02:10:33 +0530

    more tailwind work

--- app/controllers/item_types_controller.rb
+++ app/controllers/item_types_controller.rb
@@ -4,4 +4,7 @@ class ItemTypesController < ApplicationController
 		@topics = Topic.eager_load(:items).where("items.item_type_id=?", @item_type.id).order(:name).paginate(page: params[:page])
 		flash.now[:danger] = "No Topics and Items" if @topics.blank?
 	end
+
+	def index
+	end
 end
