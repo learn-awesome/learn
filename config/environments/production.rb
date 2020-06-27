09c3920beddad63a62fdfc130a42fad40e452ebe
Nilesh <git@nileshtrivedi.com>
2020-06-27 14:39:14 +0530

    Use memory_store for caching in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,8 +58,9 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
-  config.cache_store = :redis_cache_store, { url: ENV['REDISTOGO_URL'] }
-
+  # config.cache_store = :redis_cache_store, { url: ENV['REDISTOGO_URL'] }
+  conig.cache_store = :memory_store, { size: 128.megabytes }
+  
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "learn_production"
