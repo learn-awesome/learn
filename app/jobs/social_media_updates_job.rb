c43b781bf141c4ac07b6f09bd6557176bcfaec5d
Nilesh <git@nileshtrivedi.com>
2020-08-11 23:36:00 +0530

    fix in SocialMediaUpdatesJob

--- app/jobs/social_media_updates_job.rb
+++ app/jobs/social_media_updates_job.rb
@@ -4,7 +4,7 @@ class SocialMediaUpdatesJob < ApplicationJob
   def perform(*args)
     return unless ENV.has_key?("TWITTER_CONSUMER_KEY") && ENV.has_key?("TWITTER_CONSUMER_SECRET")
     begin
-      sl = User.where(auth0_uid: "twitter|1114259648326987776").first # https://learnawesome.org/users/ea1408e5-a98f-41ff-8f81-2e5d55fb034e
+      sl = SocialLogin.where(auth0_uid: "twitter|1114259648326987776").first # https://learnawesome.org/users/ea1408e5-a98f-41ff-8f81-2e5d55fb034e
       return if sl.nil?
       # post about a random topic or a random item
       topic = Topic.discover
