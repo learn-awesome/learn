7b908e3b4a6919d4628a7aa2dbd0e6a505af81be
Nilesh <git@nileshtrivedi.com>
2020-10-24 11:59:59 +0530

    WIP: SplitButtonComponent

--- app/components/split_button_component.rb
+++ app/components/split_button_component.rb
@@ -2,5 +2,9 @@ class SplitButtonComponent < ViewComponent::Base
     def initialize(item:, user:)
       @item = item
       @user = user
+      if @user
+        @lists_added = CollectionItem.where(item: @item).where(collection_id: @user.collections.pluck(:id)).pluck(:collection_id)
+        @review = Review.where(item: @item, user: @user).first || Review.new
+      end
     end
 end
