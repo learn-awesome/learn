1b863b8b8072953e53a66215b411414bf41b31d8
Clément COQUILLE <cc.lle@protonmail.com>
2021-02-19 10:35:52 +0100

    WIP #188 add note_content & improve code readability

--- app/utilities/nat_eliason.rb
+++ app/utilities/nat_eliason.rb
@@ -8,67 +8,61 @@ class NatEliason
   BASE_URL  = 'https://www.nateliason.com'
   NOTES_URL = BASE_URL + '/notes'
 
+
   def self.extract
-    notes_list_items_href_values.map do |note_href|
-      note_note_content(note_href)
+    notes.map do |note|
+      sections = note_sections(note[:href])
+      title    = sections[:top].search('h1.notes-title').first
+
+      {
+        book_title:        title.text.split(' by').first,
+        book_author_name:  title.text.split('by ').last,
+        book_cover_image:  sections[:top].search('img').attribute('src').value,
+        book_link:         is_a_book?(sections[:top]) ? get_book_link(sections[:top]) : get_article_link(sections[:top]),
+        note_author_name:  'Nat Eliason',
+        note_content:      sections[:notes].map { |e| e.text }.join(' ').gsub(/\s{2,}/, ' '),
+        note_category:     note[:category]
+      }
     end
   end
 
   private
 
-    def self.notes_list_doc
-      @notes_list_doc ||= Nokogiri::HTML(URI.open(NOTES_URL))
+    def self.notes_list
+      doc = Nokogiri::HTML(URI.open(NOTES_URL))
+      doc.search '//div[@class="notes-category"]//div[2]//div[@role="listitem"]'
     end
 
-    def self.notes_list_items
-      notes_list_doc.search '//div[@class="notes-category"]//div[2]//div[@role="listitem"]//p'
-    end
-    
-    def self.notes_list_items_href_values
-      notes_list_items.map { |element| BASE_URL + element.children.attribute('href').value }
+    def self.notes
+      notes_list.map do |element|
+        category = element.text[/\.(\w+)$/, 1]
+        href     = BASE_URL + element.at('a').attribute('href').value
+
+        Hash[category: category, href: href]
+      end
     end
 
-    def self.note_doc(url)
-      Nokogiri::HTML(URI.open(url))
-    end
-
-    def self.note_top_section(url)
-      note_doc(url).search 'div.notes-top-section'
-    end
-
-    def self.is_a_book?(url)
-      note_top_section(url).search('//a[@class="notes-link-book"]').any?
-    end
-
-    def self.get_book_link(url)
-      note_top_section(url).search('a.notes-link-book').attribute('href').value
-    end
-
-    def self.is_an_article?(url)
-      note_top_section(url).search('//a[@class="notes-link-article"]').any?
-    end
-
-    def self.get_article_link(url)
-      note_top_section(url).search('a.notes-link-article').attribute('href').value
-    end
-
-    def self.note_note_title(url)
-      note_doc(url).search('h1.notes-title').first
-    end
-
-    def self.note_note_elements(url)
-      note_doc(url).search 'div.notes-notes-section'
-    end
-
-    def self.note_note_content(url)
-      elements = note_note_elements(url)
+    def self.note_sections(url)
+      doc = Nokogiri::HTML(URI.open(url))
 
       {
-        book_title:        note_note_title(url).text.split(' by').first,
-        book_author_name:  note_note_title(url).text.split('by ').last,
-        book_cover_image:  note_top_section(url).search('img').attribute('src').value,
-        book_link:         is_a_book?(url) ? get_book_link(url) : get_article_link(url),
-        note_author_name:  'Nat Eliason',
+        top:     doc.search('div.notes-top-section'),
+        # summary: doc.search('div.notes-summary-section'),
+        # video:   doc.search('div.notes-video-section'),
+        # podcast: doc.search('div.notes-podcast-section'),
+        notes:   doc.search('div.notes-notes-section')
       }
+    end
+
+    def self.is_a_book?(section)
+      section.search('//a[@class="notes-link-book"]').any?
+    end
+
+    def self.get_book_link(section)
+      section.search('a.notes-link-book').attribute('href').value
+    end
+
+    def self.get_article_link(section)
+      section.search('a.notes-link-article').attribute('href').value
     end
 end
